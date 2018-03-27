c39d3b1ce3a588d9a13743f3905e9bf7fcfb9494
ianbara@hotmail.com <ianbara@hotmail.com>
2018-03-27 16:48:19 +0100

    Added route guards and user module

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { EventService } from '../shared/event.service'
 import { ActivatedRoute } from '@angular/router'
-
+import { IEvent } from '../shared/index'
 
 @Component({
     templateUrl: '/app/events/event-details/event-details.component.html',
@@ -13,7 +13,7 @@ import { ActivatedRoute } from '@angular/router'
 })
 
 export class EventDetailsComponent{
-    event:any
+    event:IEvent
 
     constructor(private eventService:EventService, private route: ActivatedRoute) {
     }
